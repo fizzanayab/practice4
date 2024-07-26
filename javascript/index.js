e3140b14278b3b69964bac4124b22e54c55af5df
fizza nayab <tab2baku@gmail.com>
2024-07-26 22:15:25 +0400

    updated

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,7 @@
-function menuClick(){
+function menuClick(e){
     document.getElementsByClassName('MobileNavMenu')[0].classList.toggle('MobileNavMenuDeactivate')
+    document.getElementsByClassName('fa-times')[0].classList.toggle('Xicon')
+    document.getElementsByClassName('fa-bars')[0].classList.toggle('baricon')
+ 
 }
+
